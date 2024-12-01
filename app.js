a8ea1d10893f064b06c09446fc4ab07f2dc31233
Ross Nguyen <xhieu94@gmail.com>
2024-12-01 12:06:46 +0700

    Init commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// routes
+app.get('/hello', (req, res) => {
+  res.send('Task manager app');
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} ...`);
+});
