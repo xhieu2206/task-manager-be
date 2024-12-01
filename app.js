6388b417526aff268436af2d7a941ca482d1e09b
Quỳnh Như <quynhnhu@Air-cua-Quynh.lan>
2024-12-01 16:13:28 +0700

    Setup controller and routes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
 const app = express();
+const tasks = require('./routes/task');
+
+// middleware
+app.use(express.json());
 
 // routes
 app.get('/hello', (req, res) => {
   res.send('Task manager app');
-})
+});
+
+app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
---
+++ routes/task.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getAllTasks } = require('../controllers/tasks');
+const router = express.Router();
+
+router.route('/').get(getAllTasks);
+
+module.exports = router;
