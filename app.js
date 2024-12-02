262a1e2e8dc4b4b16a2c2c22f9f67a85ba835528
Ross Nguyen <xhieu94@gmail.com>
2024-12-02 23:43:55 +0700

    111. Basic Validation

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/task');
+const connectDb = require('./db/connect');
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -14,6 +16,16 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port} ...`);
-});
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URL);
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port} ...`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,18 @@
+const Task = require('../models/Task');
+
 const getAllTasks = (req, res) => {
   res.json({
     ...req.body,
   });
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create({
+    completed: false,
+    name: req.body.name,
+  });
+
+  res.status(201).json(task);
 }
 
 const getTask = (req, res) => {
