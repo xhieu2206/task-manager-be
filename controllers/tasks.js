293d75e0520589176b1f34bd5778ca7a494d2c30
Ross Nguyen <xhieu94@gmail.com>
2024-12-07 00:28:30 +0700

    Done 114

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,54 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-  res.json({
-    ...req.body,
-  });
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+
+    res.status(200).json({
+      tasks,
+    });
+  } catch (e) {
+    res.status(500).json({
+      msg: e,
+    });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create({
-    completed: false,
-    name: req.body.name,
-  });
+  try {
+    const task = await Task.create({
+      completed: false,
+      name: req.body.name,
+    });
 
-  res.status(201).json(task);
+    res.status(201).json(task);
+  } catch (e) {
+    res.status(500).json({
+      msg: e,
+    });
+  }
 }
 
-const getTask = (req, res) => {
-  res.json({
-    id: req.params.id,
-  });
+const getTask = async (req, res) => {
+  const { id: _id } = req.params;
+
+  try {
+    const task = await Task.findOne({ _id });
+
+    if (!task) {
+      return res.status(404).json({
+        msg: `Task not found with id ${_id}`,
+      })
+    }
+
+    res.status(200).json({
+      task,
+    });
+  } catch (e) {
+    res.status(500).json({
+      msg: e,
+    });
+  }
 }
 
 const updateTask = (req, res) => {
